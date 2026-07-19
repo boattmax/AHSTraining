a41be688f2e59bacdb54fb5c7bb1813b204abed2
boattmax <boattmax@gmail.com>
2026-07-20 00:48:56 +0700

    Revert "Reapply "fix: add missing database url to prisma schema""
    This reverts commit c038a30f61d95b08adeb6682ce9697d7f155d8be.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,15 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  const connectionString = process.env.DATABASE_URL;
+  const pool = new Pool({ 
+    connectionString,
+    ssl: { rejectUnauthorized: false }
+  });
+  const adapter = new PrismaPg(pool);
+  return new PrismaClient({ adapter });
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
